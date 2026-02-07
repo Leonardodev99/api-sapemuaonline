28374efbe9ff782a26b345fb4efba5b50e53f127
Leonardo Tiago <pequenotiago2016@gmail.com>
2026-02-07 23:54:39 +0100

    Estrutura do projeto

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-import falaOi from './app';
+import app from './app';
 
-falaOi();
+const port = 3002;
+
+app.listen(port, () => {
+  console.log();
+  console.log(`Escutando na porta ${port}`);
+  console.log(`CRTL + Clique em http://localhost:${port}`);
+});
